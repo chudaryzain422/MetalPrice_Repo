0e3c0b93280cdfd03a9e3240fe22fe8475a1c2f2
Zayn <chzainmehar@gmail.com>
2023-09-24 17:02:16 +0500

    code tested sucessfully

--- metal_prices_custom/models/account_move_inherit.py
+++ metal_prices_custom/models/account_move_inherit.py
@@ -10,21 +10,18 @@ class AccountMoveInherit(models.Model):
 
     today_metal_price = fields.Float(string="Today's Metal Price")
 
+    @api.onchange('today_metal_price')
+    def set_today_metal_price(self):
+        today_date = fields.Date.today()
+        metal_price_wizard = self.env['metal.price.wizard'].search([('date', '=', today_date)], limit=1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if metal_price_wizard:
+            self.today_metal_price = metal_price_wizard.metal_price
+        elif self.today_metal_price > 0:
+            metal_price_wizard = self.env['metal.price.wizard'].create({
+                "metal_price": self.today_metal_price,
+                "date": today_date,
+            })
 
 
 
@@ -40,22 +37,34 @@ class AccountMoveLine(models.Model):
 
     metal_total = fields.Float(string="Total")
 
-    @api.depends('metal_total', 'quantity', 'move_id.today_metal_price')
+    @api.depends('metal_total', 'quantity', 'move_id.today_metal_price', 'product_id')
     def _compute_price_unit(self):
         for line in self:
-            if line.metal_total and line.quantity:
-                line.price_unit = (line.metal_total - line.price_subtotal) / line.quantity
+            if line.product_id and line.product_id.uom_id.category_id.id == 2:
+                # if line.product_id and line.product_id.uom_id.category_id.name == 'weight' and line.move_id.today_metal_price > 0:
+                #     line.price_unit = line.price_unit + line.move_id.today_metal_price
+                if line.metal_total > 0 and line.quantity > 0:
+                    line.price_unit = (line.metal_total - line.tax_ids[0].amount) / line.quantity
+                else:
+                    if line.product_id and line.move_id.today_metal_price > 0:
+                        line.price_unit = line.product_id.list_price + line.move_id.today_metal_price
+                    else:
+                        line.price_unit = line.product_id.list_price
             else:
-                line.price_unit = line.price_subtotal
+                line.price_unit = line.product_id.list_price
 
-            if line.move_id.today_metal_price:
-                line.price_unit += line.move_id.today_metal_price
+
+
 
 
     @api.onchange('metal_total', 'quantity')
     def _onchange_metal_total(self):
         if self.metal_total and not self.quantity:
             raise UserError('Quantity is required when entering a value in "Total".')
+        # if self.product_id and self.move_id.today_metal_price > 0:
+        #     self.price_unit = self.price_unit + self.move_id.today_metal_price
+        # else:
+        #     self.price_unit = self.product_id.list_price
 
 
 
--- metal_prices_custom/models/metal_price.py
+++ metal_prices_custom/models/metal_price.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, fields, models,_
 
 
-class MetalPrice(models.TransientModel):
+class MetalPrice(models.Model):
     _name = 'metal.price.wizard'
 
     metal_price = fields.Float(string="Metal Price")
     date = fields.Date(string="Date", default=lambda self: fields.Datetime.today())
+
+    def action_show_metal_prices(self):
+        # # Check if a record with the same date already exists
+        # existing_record = self.search([('date', '=', self.date)], limit=1)
+        #
+        # if existing_record:
+        #     # If a record already exists for the same date, update it
+        #     existing_record.write({'metal_price': self.metal_price})
+        # else:
+        #     # If no record exists for the same date, create a new one
+        #     self.create({'metal_price': self.metal_price, 'date': self.date})
+
+        return {
+            'name': _('Metal Prices'),
+            'view_mode': 'tree,form',
+            'res_model': 'metal.price.wizard',
+            'type': 'ir.actions.act_window',
+            'domain': [],  # You can add a domain to filter records if needed
+        }
